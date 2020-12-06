0408ed2f2045ff5360038efcc253cd7d1fa0fe0c
Falcn8 <63485306+Falcn8@users.noreply.github.com>
2020-12-05 21:10:37 -0800

    Add files via upload

---
+++ template.py
@@ -0,0 +1,5 @@
+import pyperclip # to copy
+day = # assign day# here
+f = open("day" + str(day) + "-input.txt", "r") # ==
+# PROGRAM HERE
+pyperclip.copy(success) # the answer copies to your clipbored 
